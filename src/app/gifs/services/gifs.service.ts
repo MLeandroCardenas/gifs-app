0a4cf682032362dd16ea54aed8a2dc3838b55388
mleandrocardenas <michaelcardenas199@gmail.com>
2024-01-11 21:29:35 -0500

    leer historial del localstorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,7 +11,9 @@ export class GifsService {
     private _tagsHistory:   string[] = [];
     private serviceUrl:     string = 'https://api.giphy.com/v1/gifs';
 
-    constructor( private http: HttpClient ) { }
+    constructor( private http: HttpClient ) { 
+        this.loadLocalStorage();
+     }
 
     private organizeHistory( tag: string ): void {
         if( tag.length === 0 ) return;
@@ -41,6 +43,13 @@ export class GifsService {
 
     private saveLocalStorage(): void {
         localStorage.setItem('history',  JSON.stringify(this._tagsHistory));
+    }
+
+    private loadLocalStorage(): void {
+        if( !localStorage.getItem('history') ) return;
+        this._tagsHistory = JSON.parse( localStorage.getItem('history')! );
+        if( this._tagsHistory.length === 0 ) return;
+        this.searchTag( this._tagsHistory[0] );
     }
 
     // Getters and setters
